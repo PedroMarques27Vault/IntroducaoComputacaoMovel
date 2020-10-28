a20ad12bbe6ba3603e06e045876672d867baa145
PedroMarques27 <57456759+PedroMarques27@users.noreply.github.com>
2020-10-28 20:33:29 +0000

    Added Lab_04-Retrofit and Firebase

---
+++ Lab_04-Firebase, Retrofit/SampleRetrofit/app/src/main/java/icm/aula04/sampleretrofit/MainActivity.java
@@ -0,0 +1,56 @@
+package icm.aula04.sampleretrofit;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+    ProgressDialog progressDialog;
+    private CustomAdapter customAdapter;
+    private RecyclerView recyclerview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setMessage("loading...");
+        progressDialog.show();
+
+        /*Create handle for the RetrofitInstance interface*/
+        GetDataService service = RetrofitClientInstance.getRetrofitInstance().create(GetDataService.class);
+        Call<List<RetroPhoto>> call = service.getAllPhotos();
+        call.enqueue(new Callback<List<RetroPhoto>>() {
+            @Override
+            public void onResponse(Call<List<RetroPhoto>> call, Response<List<RetroPhoto>> response) {
+                progressDialog.dismiss();
+                generateDataList(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<List<RetroPhoto>> call, Throwable t) {
+                progressDialog.dismiss();
+                Toast.makeText(MainActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    /*Method to generate List of data using RecyclerView with custom adapter*/
+    private void generateDataList(List<RetroPhoto> photoList) {
+        recyclerview = findViewById(R.id.customRecyclerView);
+        customAdapter = new CustomAdapter(this,photoList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerview.setLayoutManager(layoutManager);
+        recyclerview.setAdapter(customAdapter);
+    }
+}
